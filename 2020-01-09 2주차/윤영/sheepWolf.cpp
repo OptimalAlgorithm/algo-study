4d416debcad8c174595d01764e2ddef61380385a
조윤영 <yoon980208@gmail.com>
2020-02-01 01:09:46 +0900

    [UPDATE]양 bfs문제 풀이 수정

--- 2020-01-09 2주차/윤영/sheepWolf.cpp
+++ 2020-01-09 2주차/윤영/sheepWolf.cpp
@@ -23,86 +23,90 @@ int R = 0;
 int C = 0;
 char room[MAX_SIZE][MAX_SIZE];
 int visited[MAX_SIZE][MAX_SIZE] = {false};
+int wolf = 0;
+int sheep = 0;
 int sheepCnt = 0;
 int wolfCnt = 0;
-int tempSheepCnt = 0;
-int tempWolfCnt = 0;
 
-void dfs(int r, int c, int sheep, int wolf) {
+void bfs(int r, int c) {
     
     //방문했음을 알리고,
     visited[r][c] = true;
     
     int currentY = r;
     int currentX = c;
+    
     int nextY = 0;
     int nextX = 0;
     
-    int addSheep = 0;
-    int addWolf = 0;
-    
-    tempWolfCnt = wolf;
-    tempSheepCnt = sheep;
-    
-    bool isWay = false;
     
     for(int i=0; i<4; i++) {
+        
         nextY = currentY + moveDir[i].y;
         nextX = currentX + moveDir[i].x;
-        cout<<"["<<nextY<<"]"<<"["<<nextX<<"]"<<endl;
-        if(0<=nextY && nextY <=R && 0<=nextX && nextX <=C && room[nextY][nextX] !='#' && visited[nextY][nextX] !=true) {
-            isWay = true;
+        
+        if(0<=nextY && nextY <R && 0<=nextX && nextX <C && room[nextY][nextX] !='#' && visited[nextY][nextX] !=true) {
             if(room[nextY][nextX] == 'v') { //wolf라면,
-                cout<<"늑대 또 여기 있다"<<nextY<<","<<nextX<<endl;
-                addWolf++;
+                wolf++;
             }else if(room[nextY][nextX] == 'o'){//sheep라면,
-                cout<<"양 또 여기 있다"<<nextY<<","<<nextX<<endl;
-                addSheep++;
+                sheep++;
             }
             //빈 필드 또는 늑대 또는 양일 경우 모두 다음 장소로 이동.
-            currentY = nextY;
-            currentX = nextX;
-            
-            dfs(currentY, currentX, sheep+addSheep, wolf+addWolf);
+            bfs(nextY, nextX);
         }
     }
+    return;
+    
     //모두 return 했을 때
     // 우리의 양은 늑대에게 싸움을 걸 수 있고 영역 안의 양의 수가 늑대의 수보다 많다면 이기게 된다. 그렇지 않다면 늑대가 그 지역 안의 모든 양을 먹는다.
     
-    if(isWay == false) {//갈 수 있는 길이 더 이상 없을 때,
-        if(tempSheepCnt > tempWolfCnt) {
-            wolf = 0;
-        }else{
-            sheep = 0;
-        }
-        
-        sheepCnt += sheep;
-        wolfCnt += wolf;
-        
-        cout<<"늑대:"<<wolf<<"양"<<sheep<<endl;
-        
-        return;
-    }
+//    if(isWay == false) {//갈 수 있는 길이 더 이상 없을 때,
+//        return;
+//    }
 }
 void solution() {
     for(int i=0; i<R; i++) {
         for(int j=0; j<C; j++) {
+            wolf = 0;
+            sheep = 0;
             if(room[i][j] == 'v' && visited[i][j]!=true) {
-                cout<<"늑대 여기 있다"<<i<<","<<j<<endl;
-                dfs(i, j,0,1);
+                wolf++;
+                bfs(i, j);
+                
+                if(sheep > wolf) {
+                    wolf = 0;
+                }else{
+                    sheep = 0;
+                }
+                
+                sheepCnt += sheep;
+                wolfCnt += wolf;
+                
+                
             }else if (room[i][j] == 'o' && visited[i][j]!=true) {
-                cout<<"양 여기 있다"<<i<<","<<j<<endl;
-                dfs(i, j, 1,0);
+                sheep++;
+                bfs(i, j);
+                
+                if(sheep > wolf) {
+                    wolf = 0;
+                }else{
+                    sheep = 0;
+                }
+                
+                sheepCnt += sheep;
+                wolfCnt += wolf;
+                
             }
+
         }
+        
     }
-    cout<<"sheep:"<<sheepCnt<<endl;
-    cout<<"wolf:"<<wolfCnt<<endl;
+    cout<<sheepCnt<<" "<<wolfCnt<<endl;
     
 }
 int main(int argc, const char * argv[]) {
-    cin.tie(NULL);
-    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
     
     cin>>R>>C;
     
