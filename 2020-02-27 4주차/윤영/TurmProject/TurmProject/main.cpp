a7f1de07da13c744e4630a994b8af383db5c1a4b
조윤영 <yoon980208@gmail.com>
2020-03-05 12:49:57 +0900

    [CREATE]텀 프로젝트 84% 시간초과 (업데이트 예정 ..!)

---
+++ 2020-02-27 4주차/윤영/TurmProject/TurmProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TurmProject
+//
+//  Created by 조윤영 on 05/03/2020.
+//  Copyright © 2020 조윤영. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
