fb1d4aa454a6b47216867ebe901806c1f3ac4e8a
cheerful1313 <56524328+cheerful1313@users.noreply.github.com>
2020-03-07 02:40:33 +0900

    Update 14238_출근기록_혜민.cpp

--- 2020-03-05 1주차/혜민/14238_출근기록_혜민.cpp
+++ 2020-03-05 1주차/혜민/14238_출근기록_혜민.cpp
@@ -1,12 +1,12 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cstring>
 #include <vector>
 using namespace std;
 #define MAX 51
 
 string s;
-int memo[MAX][MAX][MAX][2][3] = { 0, }; //memo[i][j][k]는 A = i개, B = j개, C = k개 남았을 때의 출근기록. (0 = 미방문, 1 = 올바름, -1 = 올바르지않음)
-vector <char> v;
+int memo[MAX][MAX][MAX][2][3] = { 0, }; //memo[i][j][k][l][m]은 A = i개, B = j개, C = k개 남았을 때, B가 l일간 올 수 없고, C가 m일간 올 수 없을 때 출근기록. (0 = 미방문, 1 = 올바름, -1 = 올바르지않음)
+vector <char> ans;
 
 int record(int a, int b, int c, int cb, int cc) //A 개수, B 개수, C 개수, B가 못오는 일수, C가 못오는 일수
 {
@@ -22,46 +22,36 @@ int record(int a, int b, int c, int cb, int cc) //A 개수, B 개수, C 개수, 
 			memo[a - 1][b][c][ncb][ncc] = record(a - 1, b, c, ncb, ncc);
 		
 		if (memo[a - 1][b][c][ncb][ncc] > 0) //올바름 
+		{
+			ans.push_back('A');
 			return memo[a][b][c][cb][cc] = 1;
+		}
 	}
 	if (b > 0 && cb <= 0 && memo[a][b - 1][c][1][ncc] >= 0)
 	{
 		if (memo[a][b - 1][c][1][ncc] == 0)
 			memo[a][b - 1][c][1][ncc] = record(a, b - 1, c, 1, ncc);
 
-		if(memo[a][b - 1][c][1][ncc] > 0)
+		if (memo[a][b - 1][c][1][ncc] > 0)
+		{
+			ans.push_back('B');
 			return memo[a][b][c][cb][cc] = 1;
+		}
 	}
 	if (c > 0 && cc <= 0 && memo[a][b][c - 1][ncb][2] >= 0)
 	{
 		if (memo[a][b][c - 1][ncb][2] == 0) 
 			memo[a][b][c - 1][ncb][2] = record(a, b, c - 1, ncb, 2);
 
-		if(memo[a][b][c - 1][ncb][2] > 0)
+		if (memo[a][b][c - 1][ncb][2] > 0)
+		{
+			ans.push_back('C');
 			return memo[a][b][c][cb][cc] = 1;
+		}
 	}
 
 	return memo[a][b][c][cb][cc] = -1; //어디에도 걸리지 않았으면 올바르지 않음.
 }
-
-string print_record(int a, int b, int c, int cb, int cc) //record랑 똑같은데 출력용.. 왜때문에 출력 따로 해야 맞는지 모르겠음..
-{
-	int ncb = (cb - 1 <= 0) ? 0 : cb - 1;
-	int ncc = (cc - 1 <= 0) ? 0 : cc - 1;
-
-	if (a <= 0 && b <= 0 && c <= 0) //종료조건
-		return "";
-
-	if (a > 0 && memo[a - 1][b][c][ncb][ncc] > 0)
-		return "A" + print_record(a - 1, b, c, ncb, ncc);
-	if (b > 0 && cb <= 0 && memo[a][b - 1][c][1][ncc] > 0)
-		return "B" + print_record(a, b - 1, c, 1, ncc);
-	if (c > 0 && cc <= 0 && memo[a][b][c - 1][ncb][2] >= 0)
-		return "C" + print_record(a, b, c - 1, ncb, 2);
-
-	return "";
-}
-
 
 void solve()
 {
@@ -76,10 +66,7 @@ void solve()
 			c++;
 	}
 
-	if (record(a, b, c, 0, 0) < 0) //올바른 출근 기록이 없는 경우
-		cout << "-1" << endl;
-	else
-		cout << print_record(a, b, c, 0, 0) << endl;
+	record(a, b, c, 0, 0);
 }
 
 int main()
@@ -91,4 +78,14 @@ int main()
 	cin >> s;
 
 	solve();
+
+	//output
+	if (ans.empty()) //올바른 출근 기록이 없는 경우
+		cout << "-1" << endl;
+	else
+	{
+		for (int i = 0; i < ans.size(); i++)
+			cout << ans[i];
+		cout << endl;
+	}
 }
